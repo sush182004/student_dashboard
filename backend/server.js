0ec389b04f0baf49d97490548f5957850d389381
Sai Sushmitha <sushmitha182004@gmail.com>
2025-09-14 23:07:34 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('otenv').config();
+require('dotenv').config();
 
 const app = express();
 
